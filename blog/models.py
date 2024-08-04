17484f819b041af3d906c776d967e8e1d46268a2
pouriahajipour <pouriahajipour06@gmail.com>
2024-08-05 00:09:03 +0330

    wrote models for articles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=15)
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    text = models.TextField()
+    image = models.ImageField(upload_to='article')
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    banner = models.ImageField(upload_to='article' ,help_text='best size for banner:770x340')
