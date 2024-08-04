9597437ef47623f5c90cf8a2b5fc691d82e13818
pouriahajipour <pouriahajipour06@gmail.com>
2024-08-04 23:34:39 +0330

    start blog app

--- stand_blog/settings.py
+++ stand_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "blog"
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR ,'static')
-STATICFILES_DIRS = pass
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 
 
